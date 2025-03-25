82299dcff1248aeb3cc2c3eb44e1618a4fe150c2
Alfre Moracho <alfrevp@hotmail.com>
2025-03-24 21:33:34 -0300

    Agrego mensaje respuesta invalida

--- questions.py
+++ questions.py
@@ -1,4 +1,5 @@
 import random
+import sys
 
 # Preguntas para el juego
 questions = [
@@ -36,16 +37,28 @@ for _ in range(3):
 
     # El usuario tiene 2 intentos para responder correctamente
     for intento in range(2):
-        user_answer = int(input("Respuesta: ")) - 1
+        
+        input_usuario = input("Respuesta: ")
+
+        if not input_usuario.isdigit():
+            print("Respuesta no valida")
+            sys.exit(1)
+        
+        user_answer = int(input_usuario) - 1
+
+        if user_answer < 0 or user_answer >= len(correct_answers_index) - 1:
+            print("Respuesta no valida")
+            sys.exit(1)
+
         # Se verifica si la respuesta es correcta
         if user_answer == correct_answers_index[question_index]:
             print("¡Correcto!")
             break
-    else:
-        # Si el usuario no responde correctamente después de 2 intentos,
-        # se muestra la respuesta correcta
-        print("Incorrecto. La respuesta correcta es:")
-        print(answers[question_index][correct_answers_index[question_index]])
+        else:
+            # Si el usuario no responde correctamente después de 2 intentos,
+            # se muestra la respuesta correcta
+            print("Incorrecto. La respuesta correcta es:")
+            print(answers[question_index][correct_answers_index[question_index]])
 
     # Se imprime un blanco al final de la pregunta
     print()
